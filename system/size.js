faf2dacf196ac7f9870c38e7a1877510061155f2
Eugene <1lavrov1@ukr.net>
2019-05-14 11:14:44 +0300

    Added flex-basis to flex-size (equal as size) for IE 11 . Sometimes IE need flex-basis for correct width.

--- system/size.js
+++ system/size.js
@@ -1,32 +1,33 @@
-class Size{
-    
+class Size {
+
     constructor(resources, name, postfix = '') {
         this.resources = resources;
         this.name = postfix === '' ? name : name + '-' + postfix;
         this.postfix = postfix;
     }
-    
-    render(){
+
+    render() {
         let style = '';
         let mediaPostfix = this.postfix === '' ? '' : '_' + this.postfix;
 
-        if(this.resources.helpers.isPercentage(this.resources.settings.offset)){
+        if (this.resources.helpers.isPercentage(this.resources.settings.offset)) {
             style += this.resources.styles.objToCallMedia(this.postfix, {
                 width: `{{var}}atom * {{var}}n - {{var}}offset${mediaPostfix}`
             });
-        }
-        else if(this.resources.settings.detailedCalc){
+        } else if (this.resources.settings.detailedCalc) {
             style += `{{var}}val{{=}}{{i}}calc(100% / {{string-var}}columns{{/string-var}} * {{string-var}}n{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}{{;}}\n`;
-            
+
             style += this.resources.styles.objToCallMedia(this.postfix, {
                 width: '{{var}}val'
             });
-        }
-        else{
+        } else {
             style += `{{var}}val{{=}}100% / {{var}}columns * {{var}}n{{;}}\n`;
-            
+
             style += this.resources.styles.objToCallMedia(this.postfix, {
                 width: `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`
+            });
+            style += this.resources.styles.objToCallMedia(this.postfix, {
+                'flex-basis': `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`
             });
         }
 
